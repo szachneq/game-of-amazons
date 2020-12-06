00dc12df390c60706156083f501c5515462e1c5e
Mateusz Jakubowski <01158911@pw.edu.pl>
2020-12-06 18:57:14 +0100

    feat: prestenboard()

--- Lib/Libinteractive.c
+++ Lib/Libinteractive.c
@@ -1,46 +1,56 @@
 #include "Libinteractive.h"
+#include "Variables.h"
 
 void generateBoard(Field g_board[BOARD_SIZE][BOARD_SIZE]) 
 {
     // printf("Generating game board...\n");
     // populate the board randomly
     // printf("Board generated!\n");
+    
 }
 void presentBoardState(Field g_board[BOARD_SIZE][BOARD_SIZE]) {
 
-    // present board state to the user
-    // for(int row = 0; row <= BOARD_SIZE; ++row){
-    //     for(int column = 0; column <= BOARD_SIZE; ++column){
+    //present board state to the user
+    for(int row = 0; row <= BOARD_SIZE; row++){
+        for(int column = 0; column <= BOARD_SIZE; column++){
 
-    //         if(row == 0  ){
-    //             if(column == 0){
-    //                 printf("| %d  ", column);
-    //             }else{
-    //                 printf("| %d ", column);
-    //              }
+            if(row == 0){
+              
+                if(column == 0){
+                    printf("| %d ", column);
+                }else{
+                    printf("|   %d   ", column);
+                 }
                 
-    //         }else if(column == 0 && row !=0){
+            }else if(column == 0 && row !=0){
+                if(row == 10){
+                    printf("| %d", row);
+                }
+                else if(row < 10){
+                    
+                 printf("| %d ", row);
+                 }
+                 else{
+                     printf("| %d ", row);
+                 }
 
-    //             if(row < 10){
-    //              printf("| %d  ", row);
-    //              }else{
-    //                  printf("| %d ", row);
-    //              }
-
-    //         }
-
-    //         if(row > 0 && column > 0){
-    //             printf("| %d ", g_board[row][column]);
-    //         }
+            }
             
-    //     }
-    //     if(row == 0){ 
-    //         printf("|\n");
-    //     }
-    //     else{
-    //         printf(" |\n");
-    //     }        
-    // }
+
+            if(row > 0 && column > 0){
+               
+                printf("| %d %d %d ", g_board[row][column].playerID, g_board[row][column].value, g_board[row][column].artifact);
+
+            }
+            
+        }
+        if(row == 0){ 
+            printf("|\n");
+        }
+        else{
+            printf(" |\n");
+        }        
+    }
 }
 void switchPlayer(int *player) 
 {
