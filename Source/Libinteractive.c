11a1d0af4ad5d1f5d964bd72f2289fcd225ceba2
Mateusz Jakubowski <01158911@pw.edu.pl>
2021-01-20 21:31:36 +0100

    feat: Comments

--- Source/Libinteractive.c
+++ Source/Libinteractive.c
@@ -12,7 +12,9 @@
  * @brief Function which generates the board and randomly sets artifacts and treasures.
  * 
  * @param game structure which contains all variables used in the current game state, this function uses variables holding: board.
+ * 
  * @note Field board included in game structure holds player's ID, value of a treasure and type of artifact.
+ * 
  * @warning Randomly choosen values and artifacts have adjusted suitable frequency.
  */
 void generate_board(Game *game) {
@@ -45,7 +47,13 @@ void generate_board(Game *game) {
 
     printf("Board generated!\n");
 }
-
+/**
+ * @brief Function prints generated board.
+ * 
+ * @param game structure which contains all variables used in the current game state, this function uses variables holding: board, g_scores.
+ * @note Field board included in game structure holds player's ID, value of a treasure and type of artifact.
+ * @note int g_scores displays held in the structure amount of points collected by the current player.
+ */
 void present_board_state(Game *game) {
     // print the state of the board in console
     for(int row = 0; row <= BOARD_SIZE; row++) {
@@ -74,7 +82,12 @@ void present_board_state(Game *game) {
     }
     printf("Score: Player1 - %d, Player2 - %d\n", game->g_scores[0], game->g_scores[1]);
 }
-
+/**
+ * @brief Function switches between players.
+ * 
+ * @param current_player we use a pointer to get to the value.
+ * @note Function switch_player is always called by a different function which takes the game structure.
+ */
 void switch_player(int *current_player) 
 {
     if (*current_player == 1)
@@ -87,13 +100,28 @@ void switch_player(int *current_player)
 }
 
 
-
+/**
+ * @brief Function places an amazon.
+ * 
+ * @param game structure which contains all variables used in the current game state, this function uses variables holding: board.
+ * @param player current player's ID which is switched by switch_player function.
+ * 
+ */
 void place_amazon(Game *game, int player) {
   game->board[game->p.y][game->p.x].playerID = player;
   game->board[game->p.y][game->p.x].value = 0;
   game->board[game->p.y][game->p.x].artifact = 0;
 }
 
+/**
+ * @brief One of the leading functions, it asks the user about amount of amazons and coordinates on which the player places the amazons.
+ * 
+ * @param game structure which contains all variables used in the current game state, this function uses variables holding: board, p.
+ * 
+ * @note Field board included in game structure holds player's ID, value of a treasure and type of artifact.
+ * @note Position p included in game structure holds amazons coordinates.
+ * 
+ */
 void init_placement(Game *game) {
     
     int amazons = 0;
