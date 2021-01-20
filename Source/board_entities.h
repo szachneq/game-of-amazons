4d9f1ed492dfa5100a5d6bfcba782a939f586d87
Jan Szachno <j.szachno@gmail.com>
2021-01-20 21:03:02 +0100

    doc board_entities.h

--- Source/board_entities.h
+++ Source/board_entities.h
@@ -1,6 +1,12 @@
 #ifndef BOARD_ENTITIES_H
 #define BOARD_ENTITIES_H
 
+/// This file contains structures used in autonomous game.
+
+/**
+ * @brief enum variable containing all possible types of artifacts on the board
+ * 
+ */
 typedef enum {
   NONE = 0,
   HORSE = 1,
@@ -8,12 +14,20 @@ typedef enum {
   SPEAR = 3,
 } EArtifact;
 
+/**
+ * @brief structure describing fields on the board
+ * 
+ */
 typedef struct {
   int value;
   EArtifact artifact;  
   int player_id;
 } Field;
 
+/**
+ * @brief structure describing coordinates on the board
+ * 
+ */
 typedef struct {
   int x;
   int y;
